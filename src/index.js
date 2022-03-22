ba67b250e44aca32801104cba7b2f9b1f658fce5
Guilherme Gomes David <guidev001@gmail.com>
2022-03-22 20:51:01 -0300

    Finalização do desafio

--- src/index.js
+++ src/index.js
@@ -23,22 +23,25 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { title, url, techs } = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  const repository = repositories.find((repository) => repository.id === id);
 
-  if (repositoryIndex < 0) {
-    return response.status(404).json({ error: "Repository not found" });
+  if(!repository) {
+    return response.status(404).json({error: "Repository not found!"});
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs;
 
-  repositories[repositoryIndex] = repository;
 
   return response.json(repository);
 });
@@ -48,7 +51,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -66,9 +69,11 @@ app.post("/repositories/:id/like", (request, response) => {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const likes = ++repositories[repositoryIndex].likes;
+  ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  const likes = repositories[repositoryIndex].likes;
+  
+  return response.json({likes});
 });
 
 module.exports = app;
